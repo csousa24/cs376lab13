6b1b0ddb165594312a64309c5390eef484de4efc
csousa24 <sousa24@up.edu>
2024-12-13 13:31:52 +0000

    added button controls to the left panel

--- WindowExample.java
+++ WindowExample.java
@@ -31,13 +31,16 @@ public class WindowExample extends JFrame{
         //JLabel labelTop = new JLabel("You are a CS376 rockstar! Today:" + addTime());
 
         //1. left panel creation here
+	leftPanel = new JPanel();
+leftPanel.setLayout(new BoxLayout(leftPanel,0));
 
         //2. bottom pane creation here
 	bottomPanel= new JLayeredPane();
 bottomPanel.setPreferredSize(new Dimension(600, 600));
 
-
         //3. happy and sad image buttons
+	buttonHappy = new JButton(new HappyAction());
+buttonSad = new JButton(new SadAction());
 
         //4. happy and sad button change image
 
@@ -64,10 +67,13 @@ scrollPaneSad.setBounds(10, 50, imgSad.getIconWidth()+10, imgSad.getIconHeight()
         areaPanel.add(BorderLayout.PAGE_START,topPanel);
 
         //9. add buttons Happy and Sad to left panel
+	leftPanel.add(buttonHappy);
+leftPanel.add(buttonSad);
 
         //10. add buttons OK and NOT to left panel
 
         //11. add left panel to frame
+	areaPanel.add(BorderLayout.LINE_START,leftPanel);
 
         //12. add images to bottom panel
 	bottomPanel.add(labelBottom);
@@ -91,4 +97,19 @@ bottomPanel.add(scrollPaneSad);
 }
 
 //16. Happy and Sad Action listeners implemetation 
+	class HappyAction extends AbstractAction{
+  HappyAction(){ super("Good"); }
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    System.out.println("Good");
+  }
+}
+
+class SadAction extends AbstractAction{
+  SadAction(){ super("Sad"); }
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    System.out.println("Noooot Good");
+  }
+}
 
